278c8b381588db3ed591cf8857bb439b3a89a3d1
Yossi Elkrief <elkriefy@gmail.com>
2018-04-13 17:15:44 +0300

    Update MainActivity.kt

--- app/src/main/java/com/bq/openglcamera/MainActivity.kt
+++ app/src/main/java/com/bq/openglcamera/MainActivity.kt
@@ -1,18 +1,18 @@
 package com.bq.openglcamera
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Camera
 import android.graphics.SurfaceTexture
 import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
 import android.hardware.camera2.CaptureRequest
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
 import android.os.Looper
+import android.support.v7.app.AppCompatActivity
 import android.view.Surface
 import android.view.TextureView
 import com.bq.openglcamera.opengl.DefaultCameraRenderer
@@ -30,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var textureView: TextureView
     var surfaceTexture: SurfaceTexture? = null
 
-    val backgroundThread = HandlerThread("bg")
+    private val backgroundThread = HandlerThread("bg")
     lateinit var backgroundHandler: Handler
 
     lateinit var textureViewGLWrapper: TextureViewGLWrapper
@@ -39,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textureView = findViewById(R.id.texture_view) as TextureView
+        textureView = findViewById(R.id.texture_view)
 
         val defaultCameraRenderer = DefaultCameraRenderer(this)
 
@@ -49,8 +49,6 @@ class MainActivity : AppCompatActivity() {
             openCamera()
         }, Handler(Looper.getMainLooper()))
 
-        backgroundThread.start()
-        backgroundHandler = Handler(backgroundThread.looper)
 
         textureView.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
             override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
@@ -71,6 +69,16 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        backgroundThread.start()
+        if (!::backgroundHandler.isInitialized) {
+            backgroundHandler = Handler(backgroundThread.looper)
+        }
         RxPermissions(this)
                 .request(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO)
                 .subscribe { granted ->
@@ -83,16 +91,17 @@ class MainActivity : AppCompatActivity() {
                 }
     }
 
-    override fun onResume() {
-        super.onResume()
-        openCamera()
-    }
-
     override fun onPause() {
         super.onPause()
         closeCamera()
+        try {
+            backgroundThread.interrupt()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
+    @SuppressLint("MissingPermission")
     private fun openCamera() {
         if (!canOpenCamera) return
         if (!textureView.isAvailable) return
@@ -102,7 +111,6 @@ class MainActivity : AppCompatActivity() {
         cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
         cameraManager!!.openCamera("0", object : CameraDevice.StateCallback() {
             override fun onOpened(camera: CameraDevice) {
-                this@MainActivity.cameraDevice = cameraDevice
                 this@MainActivity.surface = Surface(surfaceTexture)
                 surfaceTexture?.setDefaultBufferSize(textureView.width, textureView.height)
                 val req = camera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
@@ -110,7 +118,7 @@ class MainActivity : AppCompatActivity() {
 
                 camera.createCaptureSession(listOf(surface), object : CameraCaptureSession.StateCallback() {
                     override fun onConfigured(session: CameraCaptureSession) {
-                        req.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE)
+                        req.set(CaptureRequest.CONTROL_AF_MODE, CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_VIDEO)
                         req.set(CaptureRequest.CONTROL_AE_MODE, CaptureRequest.CONTROL_AE_MODE_ON)
                         req.set(CaptureRequest.CONTROL_AE_ANTIBANDING_MODE, CaptureRequest.CONTROL_AE_ANTIBANDING_MODE_AUTO)
                         session.setRepeatingRequest(req.build(), null, null)
